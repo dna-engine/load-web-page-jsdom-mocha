6c818f5e3688cd92446bef11528c36d45291ecc2
Dem Pilafian <dem@centerkey.com>
2020-01-07 22:15:53 -0800

    🚀 flying to 🪐

--- spec.js
+++ spec.js
@@ -1,45 +1,43 @@
 // Mocha Specification Cases
 
 // Imports
-const assert =    require('assert').strict;
-const { JSDOM } = require('jsdom');
+const assert =          require('assert').strict;
+const serverListening = require('server-listening');
+const { JSDOM } =       require('jsdom');
 
 // Setup
-const url  = 'https://dragonsgrill.org/';
-let window, $;
-const loadWebPage = (done) => {
-   const handleWebPage = (dom) => {
-      const waitForScripts = () => {
-         window = dom.window;
-         $ = dom.window.jQuery;
-         done();
-         };
-      dom.window.onload = waitForScripts;
-      };
-   const options = { resources: 'usable', runScripts: 'dangerously' };
-   JSDOM.fromURL(url, options).then(handleWebPage);
-   };
-const closeWebPage = () => window.close();
+const url = 'https://pretty-print-json.js.org/';
+const jsdomOptions = { resources: 'usable', runScripts: 'dangerously' };
+let dom;
+before(() => JSDOM.fromURL(url, jsdomOptions)
+   .then(serverListening.jsdomOnLoad)
+   .then((jsdom) => dom = jsdom)
+   );
+after(() => dom.window.close());
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 describe('The web page', () => {
 
-   before(loadWebPage);
-   after(closeWebPage);
-
    it('has the correct URL -> ' + url, () => {
-      const actual =   { url: window.location.href };
+      const actual =   { url: dom.window.location.href };
       const expected = { url: url };
       assert.deepEqual(actual, expected);
       });
 
    it('has exactly one header, main, and footer', () => {
       const actual =   {
-          header: $('body >header').length,
-          main:   $('body >main').length,
-          footer: $('body >footer').length
-          };
+         header: dom.window.$('body >header').length,
+         main:   dom.window.$('body >main').length,
+         footer: dom.window.$('body >footer').length
+         };
       const expected = { header: 1, main: 1, footer: 1 };
+      assert.deepEqual(actual, expected);
+      });
+
+   it('has a 🚀 flying to 🪐!', () => {
+      const html = dom.window.document.documentElement.outerHTML;
+      const actual =   { '🚀': !!html.match(/🚀/g), '🪐': !!html.match(/🪐/g) };
+      const expected = { '🚀': true,                '🪐': true };
       assert.deepEqual(actual, expected);
       });
 
